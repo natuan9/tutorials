23bddbdec5cc9a3a0590a838beccf6cc8eadeab3
natuan9 <tuan3032001@gmail.com>
2024-11-16 23:03:25 +0700

    [IMP] awesome_owl: 6. The sum of two Counter

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -4,10 +4,17 @@ export class Counter extends Component {
     static template = "awesome_owl.counter";
 
     setup() {
-        this.state = useState({value: 0});
+        this.state = useState({value: 1});
     }
+
+    static props = {
+        onChange: {type: Function, optional: true}
+    };
 
     increment() {
         this.state.value++
+        if(this.props.onChange){
+            this.props.onChange();
+        }
     }
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -7,7 +7,15 @@ import { Card } from "./card/card";
 export class Playground extends Component {
     static template = "awesome_owl.playground";
 
+    setup() {
+        this.sum = useState({value: 2})
+    }
+
     htmlContent = markup("<div class='text-primary'>some content</div>");
+
+    incrementSum() {
+        this.sum.value++;
+    }
 
     static components = { Counter, Card };
 }
