3a5f3e08d152144ad615b749c0f31b2908466af9
natuan9 <tuan3032001@gmail.com>
2024-11-17 10:46:16 +0700

    [IMP] awesome_owl: 7. A todo list

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -3,6 +3,7 @@
 import { Component, useState, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
 import { Card } from "./card/card";
+import { TodoList } from "./todo_list/todo_list";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
@@ -17,5 +18,5 @@ export class Playground extends Component {
         this.sum.value++;
     }
 
-    static components = { Counter, Card };
+    static components = { Counter, Card, TodoList };
 }
