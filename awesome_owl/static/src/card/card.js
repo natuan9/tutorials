3962b92b6d822a72b8a52600ac402479a8d66e8f
natuan9 <tuan3032001@gmail.com>
2024-12-29 09:58:42 +0700

    [IMP] awesome_owl: 14. Minimizing card content

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -3,6 +3,10 @@ import { Component, useState } from "@odoo/owl";
 export class Card extends Component {
     static template = "awesome_owl.card"
 
+    setup() {
+        this.state = useState({isOpen: true});
+    }
+
     static props = {
         title: {type: String},
         slots: {
@@ -12,4 +16,8 @@ export class Card extends Component {
             },
         }
     };
+
+    toggleContent() {
+        this.state.isOpen = !this.state.isOpen
+    }
 }
