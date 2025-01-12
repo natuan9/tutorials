1bfdbc946abedbb03dce288d5ef266be704bae93
natuan9 <tuan3032001@gmail.com>
2025-01-12 10:40:44 +0700

    [IMP] awesome_clicker: 3. Create a client action

---
+++ awesome_clicker/static/src/client_action/client_action.js
@@ -0,0 +1,7 @@
+import { Component } from "@odoo/owl";
+import { registry } from "@web/core/registry";
+class ClickerClientAction extends Component {
+    static template = "awesome_clicker.ClickerClientAction";
+    static props = ['*'];
+}
+registry.category("actions").add("awesome_clicker.client_action", ClickerClientAction);
