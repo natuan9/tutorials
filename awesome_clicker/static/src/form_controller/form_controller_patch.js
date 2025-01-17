5dd19f1062a41bc7a9e9b053d2e32e582b615c42
natuan9 <tuan3032001@gmail.com>
2025-01-17 23:20:20 +0700

    [IMP] awesome_clicker: 14. Provide a reward when opening a form view

---
+++ awesome_clicker/static/src/form_controller/form_controller_patch.js
@@ -0,0 +1,14 @@
+import { FormController } from "@web/views/form/form_controller";
+import { patch } from "@web/core/utils/patch";
+import { useClicker } from "../clicker_hook";
+const FormControllerPatch = {
+    setup() {
+        super.setup(...arguments);
+        const clicker = useClicker();
+        console.log("Hello world");
+        if (Math.random() < 0.01) {
+            clicker.giveReward();
+        }
+    },
+};
+patch(FormController.prototype, FormControllerPatch);
