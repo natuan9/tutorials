534bd0ca4f25ad43f06f1a80e6fbe4e56c564c7a
natuan9 <tuan3032001@gmail.com>
2025-02-09 13:02:37 +0700

    [IMP] awesome_gallery: 2. Use the Layout component

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,9 +1,11 @@
 import { Component } from "@odoo/owl";
 import { standardViewProps } from "@web/views/standard_view_props";
+import { Layout } from "@web/search/layout";
 
 export class GalleryController extends Component {
     static template = "awesome_gallery.GalleryController";
     static props = {
         ...standardViewProps,
     };
+    static components = { Layout };
 }
